87c379683c126802f46701b9f03b1881b9e5f33f
Adam Biggs <adam.biggs@lightmaker.com>
2017-01-18 17:44:28 -0800

    feat: on sync, delete existing state and repopulate from Cognito storage

--- src/mutations.js
+++ src/mutations.js
@@ -8,6 +8,11 @@ export default {
     delete state[key]
   },
   [types.SYNC] (state, { records }) {
+    // Delete existing keys - Cognito datastore is the source of truth
+    for (let key in state) {
+      delete state[key]
+    }
+    // Assign keys from Cognito datastore
     for (let record of records) {
       state[record.key] = record.value
     }
