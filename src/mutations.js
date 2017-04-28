99c8ffa474edcba564cbf23d56b2f30cad63989c
Adam Biggs <adam.biggs@lightmaker.com>
2017-04-28 12:03:49 -0700

    fix: values sometimes don't update when accessed via a Vuex getter

--- src/mutations.js
+++ src/mutations.js
@@ -1,23 +1,24 @@
+import Vue from 'vue'
 import * as types from './mutation-types'
 
 export default {
   [types.PUT] (state, { key, value }) {
-    state[key] = value
+    Vue.set(state, key, value)
   },
 
   [types.REMOVE] (state, { key }) {
-    delete state[key]
+    Vue.delete(state, key)
   },
 
   [types.SYNC] (state, { records }) {
     // Delete existing keys - Cognito datastore is the source of truth
-    for (let key in state) {
-      delete state[key]
+    for (const key in state) {
+      Vue.delete(state, key)
     }
     // Assign keys from Cognito datastore
-    for (let record of records) {
+    for (const record of records) {
       if (!record.value) continue
-      state[record.key] = record.value
+      Vue.set(state, record.key, record.value)
     }
   }
 }
--- test/mutations.js
+++ test/mutations.js
@@ -1,6 +1,8 @@
 import test from 'ava'
+import sinon from 'sinon'
 
 // Modules to test
+import Vue from 'vue'
 import * as types from '../src/mutation-types'
 import mutations from '../src/mutations'
 
@@ -8,25 +10,34 @@ import mutations from '../src/mutations'
 let state
 
 test.beforeEach(t => {
+  t.context.sb = sinon.sandbox.create()
+  t.context.sb.spy(Vue, 'set')
+  t.context.sb.spy(Vue, 'delete')
   state = {}
 })
 
-test('PUT mutation', t => {
+test.afterEach.always(t => {
+  t.context.sb.restore()
+})
+
+test.serial('PUT mutation', t => {
   let key = 'key'
   let value = 'value'
   mutations[types.PUT](state, { key, value })
   t.is(state[key], value)
+  sinon.assert.calledWith(Vue.set, state, key, value)
 })
 
-test('REMOVE mutation', t => {
+test.serial('REMOVE mutation', t => {
   let key = 'key'
   let value = 'value'
   state[key] = value
   mutations[types.REMOVE](state, { key })
   t.is(state[key], undefined)
+  sinon.assert.calledWith(Vue.delete, state, key)
 })
 
-test('SYNC mutation', t => {
+test.serial('SYNC mutation', t => {
   state.oldKey = 'old value'
   let records = [
     {
@@ -46,4 +57,8 @@ test('SYNC mutation', t => {
   t.is(state.oldKey, undefined, 'clears records that were removed from Cognito Sync storage')
   t.is(state.key1, 'value1')
   t.is(state.key2, 'value2')
+  sinon.assert.calledWith(Vue.delete, state, 'oldKey')
+  sinon.assert.callCount(Vue.set, 2)
+  sinon.assert.calledWith(Vue.set, state, records[1].key, records[1].value)
+  sinon.assert.calledWith(Vue.set, state, records[2].key, records[2].value)
 })
